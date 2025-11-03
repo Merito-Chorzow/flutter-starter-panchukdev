c8a73d2a104b375f4d29af97c27e3288abcce3a9
Yaroslav Panchuk <panchukdev@protonmail.com>
2025-11-03 09:31:45 +0100

    readme

--- flutter_app/server.py
+++ flutter_app/server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # вот эта строка позволяет Flutter Web обращаться к API
+CORS(app)  # flutter web communication with api
 
 entries = []
 
